249e43f0646dce0948c88c7e7ac3d6f924567e5c
Egemen <30879498+erogluegemen@users.noreply.github.com>
2022-03-06 19:33:30 +0300

    Update music_cog.py

--- music_cog.py
+++ music_cog.py
@@ -1,7 +1,9 @@
+import time
 import discord
 from discord.ext import commands
 import ffmpeg
 from youtube_dl import YoutubeDL
+from lyricsgenius import Genius
 
 
 class music_cog(commands.Cog):
@@ -14,7 +16,8 @@ class music_cog(commands.Cog):
                                'options': '-vn'}
         self.vc = ""
 
-    # Youtube sorgusu
+#Get Song
+
     def search_yt(self, item):
         with YoutubeDL(self.YDL_OPTIONS) as ydl:
             try:
@@ -24,21 +27,21 @@ class music_cog(commands.Cog):
 
         return {'source': info['formats'][0]['url'], 'title': info['title']}
 
+#Skip Function
+
     def play_next(self):
         if len(self.music_queue) > 0:
             self.is_playing = True
 
-            # Parçanı URL sini alıyoruz
             m_url = self.music_queue[0][0]['source']
-
-            # Çalan şarkıyı remove etmek
             self.music_queue.pop(0)
-
             self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS, executable="ffmpeg.exe"),
                          after=lambda e: self.play_next())
+
         else:
             self.is_playing = False
 
+#Play Function
 
     async def play_music(self):
         if len(self.music_queue) > 0:
@@ -48,6 +51,7 @@ class music_cog(commands.Cog):
 
             if self.vc == "" or not self.vc.is_connected() or self.vc == None:
                 self.vc = await self.music_queue[0][1].connect()
+
             else:
                 await self.vc.move_to(self.music_queue[0][1])
 
@@ -56,30 +60,69 @@ class music_cog(commands.Cog):
 
             self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS, executable="ffmpeg.exe"),
                          after=lambda e: self.play_next())
+
         else:
             self.is_playing = False
+
+#Play Command
 
     @commands.command(aliases=["p"],name="play", help="Youtubeda bulduğu şarkıyı açar")
     async def p (self, ctx, *args):
         query = " ".join(args)
 
         voice_channel = ctx.author.voice.channel
+        #await voice_channel.connect()
+
         if voice_channel is None:
             await ctx.send("Bir ses kanalına bağlanın!")
         else:
             song = self.search_yt(query)
             if type(song) == type(True):
-                await ctx.send("Şarkı sıraya eklendi!")
+                await ctx.send("Sıkıntılar yaşadım. Bir daha denesene bi")
 
             else:
-                #bunda bir bozukluk var gerek de yok pek ctx send atmaya.
-                # await ctx.send("Şarkı indirilemedi. Yanlış biçim başka bir anahtar kelime deneyin. Bunun nedeni oynatma listesi veya canlı yayın biçimi olabilir.")
                 self.music_queue.append([song, voice_channel])
-
+                await ctx.send("{} sıraya eklendi!".format(song['title']))
                 if self.is_playing == False:
                     await self.play_music()
 
-    @commands.command(name="list", help="Mevcut müzik sırasını görüntüler")
+#Pause Command
+    @commands.command(name="pause", help="Mevcut çalan şarkıyı durdurur.")
+    async def pause(self, ctx):
+        voice = discord.utils.get(self.vc, guild=ctx.guild)
+        if voice.is_connected():
+            await voice.pause()
+            await ctx.send("Şarkı durduruldu.")
+
+        else:
+            await ctx.send("Şu anda oynatılan bir şarkı yok")
+
+#Resume Command
+    @commands.command(name="resume", help="Durdurulan şarkıyı devam ettirir.")
+    async def resume(self,ctx):
+        voice = discord.utils.get(self.vc, guild=ctx.guild)
+        if voice.is_paused():
+            voice.resume()
+            await ctx.send("Şarkı devam ediyor.")
+
+        else:
+            await ctx.send("Durdurulanan bir şarkı yok.")
+
+
+#List Command
+
+    @commands.command(aliases=["l"],name="list", help="Mevcut müzik sırasını görüntüler")
+    async def l(self, ctx):
+        retval = ""
+        for i in range(0, len(self.music_queue)):
+            retval += self.music_queue[i][0]['title'] + "\n"
+
+        print(retval)
+        if retval != "":
+            await ctx.send(retval)
+        else:
+            await ctx.send("Sırada hiç müzik yok!")
+    # *****************************************************
     async def list(self, ctx):
         retval = ""
         for i in range(0, len(self.music_queue)):
@@ -91,9 +134,39 @@ class music_cog(commands.Cog):
         else:
             await ctx.send("Sırada hiç müzik yok!")
 
-    @commands.command(name="skip", help="Çalınmakta olan şarkıyı atlar")
-    async def skip(self,ctx):
+#Skip Command
+
+    @commands.command(aliases=["s"], name="skip", help= "Müziği geçer")
+    async def s(self, ctx):
         if self.vc != "" and self.vc:
-            self.vc.stop()
-            # varsa sıradaki oynamaya çalışsın
-            await self.play_music()
+            try:
+                self.vc.stop()
+                await ctx.play_music()
+            except:
+                pass
+    # *****************************************************
+    async def skip(self, ctx):
+        if self.vc != "" and self.vc:
+            try:
+                self.vc.stop()
+                await ctx.play_music()
+            except:
+                pass
+
+#Show Lyrics Command
+
+    @commands.command(name="lyrics", help="Şarkı sözlerini gösterir", pass_context=True)
+    async def lyrics(self, ctx,*, artist_song):
+        genius_key = 'z0QcIpr0qEMVX9TrGhTB3zemYr9lxa3QyUvcY06UkFt0TmqWzYyvXIld1azo04C1'
+        genius = Genius(genius_key)
+        artist1, song1 = artist_song.split("-")
+        print("Şarkıcı: {}\nŞarkı: {}".format(artist1,song1)) #terminalde görmem için test amaçlı
+        song = genius.search_song(song1, artist1)
+        lyrics_first_part = song.lyrics[0:len(song.lyrics) // 2]
+        lyrics_second_part = song.lyrics[len(song.lyrics) // 2 if len(song.lyrics) % 2 == 0 else ((len(song.lyrics) // 2) + 1):]
+        emb = discord.Embed(title="{} - {}".format(artist1,song1),color=0xa3a3ff)
+        emb.add_field(name="Field 2 Title", value=f"{lyrics_first_part}", inline=True)
+        emb.add_field(name="Field 3 Title", value=f"{lyrics_second_part}", inline=True)
+        await ctx.send(embed=emb)
+        #description=f"{song.lyrics}"
+
