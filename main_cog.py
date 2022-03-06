02f2ece8c6f5d441d083179cdb8543cd61b26a59
Egemen <30879498+erogluegemen@users.noreply.github.com>
2022-03-06 19:36:00 +0300

    Update main_cog.py

--- main_cog.py
+++ main_cog.py
@@ -1,59 +1,91 @@
+
 import discord
 from discord.ext import commands
+from random import choice
+
 
 class main_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.help_message = """
 ```
-Genel Komutlar:
--help -> Bütün komutları gösterir
--disconnect -> Botu geldiği yere geri gönderir.
 
-Müzik Komutları:
--p <şarkı adı / link> -> Youtubeda bulduğu şarkıyı açar
--list -> Mevcut müzik sırasını görüntüler
--skip -> Çalınmakta olan şarkıyı atlar
+## Genel Komutlar ##
+-help -> Bütün komutları gösterir.
+-disconnect / dc -> Botu geldiği yere geri gönderir.
+-ping -> pingi kontrol eder.
+-clear -> belirtilen kadar mesajı siler.
+
+## Müzik Komutları ##
+-p {şarkı adı / link} -> Youtubeda bulduğu şarkıyı açar.
+-l / list -> Mevcut müzik sırasını görüntüler.
+-s / skip -> Çalan şarkıyı atlar.
+-lyrics {şarkıcı - şarkı adı}-> Şarkının sözlerinin gösterir.
+
+## Mesaj Komutları ##
+-siir -> Rastgele bir şiir verir.(Büyük ihtimal küfürlü)
+-soz -> Panky şekli şemali misali günün sözünü verir.(Küfürlü olma olasılığı var)
+-recommend ->Söylenilen filme benzer 10 film önerir.
+
+## Şans Komutları ##
+-coinflip -> Yazı tura atar.
+-dice -> Zar atar.
+-rps -> Taş-Kağıt-Makas yapar.
+-rr -> Rus ruleti.
+-sor -> Merak ettiğin şeyi sor cevaplasın.
+
 ```
 """
-        self.text_channel_list = []
+        self.text_channel_list = [""] #kendi channel_id'niz.
 
-    #Botun bağlandığında çalışacak bölüm
+#Connect
+
     @commands.Cog.listener()
     async def on_ready(self):
+        print(f'Yine Geldik Mekana {self.bot.user} ({self.bot.user.id})')
         for guild in self.bot.guilds:
             for channel in guild.text_channels:
                 self.text_channel_list.append(channel)
 
+#Reconnect
 
+    @commands.Cog.listener()
+    async def on_resumed(self):
+        print('Arap Şükrü Tekrar Bağlandı!')
 
-    @commands.command(name="help", help="Bütün komutları gösterir", pass_context=True)
+#Error Handling
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        error_messages = ["Biz böyle bir talimat vermedik!","Sen ne diyon??","Kardeşimsin bu ne demek","Bir daha yaz anlamadım"]
+
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send(choice(error_messages))
+
+        elif isinstance(error, commands.MissingPermissions):
+            await ctx.send('Sana böyle bi yetki verdiğimi hatırlamıyorum!')
+
+#Help
+
+    @commands.command(name="help", help="Bütün komutları gösterir.", pass_context=True)
     async def help(self, ctx):
         await ctx.send(self.help_message)
 
-    async def send_to_all(self, msg):
-        if text_channel in self.text_channel_list:
-            self.channel = commands.get_channel(891700345997262919)
-            #ÖNEMLİ !!!!! yukarıdaki metin kanalı benim serverimdeki metin kanalının idsi siz de kendinizinkini yapmanız lazım.
-            #get_channel kısmında mesajın yazılmasını istediğiniz kanalın id sini kopyalayın
-            await self.channel.send(self.help_message)
+#Disconnect
 
-
-    #EĞER HER KANAL IDSINI ALAMADIYSANIZ YA DA MESAJIN HER METIN KANALINA GİTMESİNİ İSTİYORSANIZ:
-    #YUKARIDAKİ KOD BLOĞUNU SİLİP AŞAĞIDAKİ KOD BLOĞUNU AKTİVE EDİN
-
-    # @commands.command(name="help", help="Bütün komutları gösterir")
-    # async def help(self, ctx):
-    #     await ctx.send(self.help_message)
-    #
-    # async def send_to_all(self, msg):
-    #     for text_channel in self.text_channel_list:
-    #         await text_channel.send(msg)
-
-
-    @commands.command(name="disconnect", help="Serverdan ayrılır", pass_context=True)
+    @commands.command(name="disconnect", help="Serverdan ayrılır.", pass_context=True)
     async def disconnect(self, ctx):
+        disconnect_messages = ["Hoççağalın..","Ben Kaçar","Gittim o zaman","İstenmediğim yerde durmam"]
+        await ctx.send(choice(disconnect_messages))
         await ctx.voice_client.disconnect()
+
+
+    @commands.command(name="dc", help="Serverdan ayrılır.", pass_context=True)
+    async def dc(self, ctx):
+        disconnect_messages = ["Hoççağalın..", "Ben Kaçar", "Gittim o zaman", "İstenmediğim yerde durmam"]
+        await ctx.send(choice(disconnect_messages))
+        await ctx.voice_client.disconnect()
+
 
 
 
